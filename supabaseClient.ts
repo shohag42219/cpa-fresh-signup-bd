18fc34d6310112828eab77fafbb0726fd2604cbf
shohag42219 <phoneoppo2219@gmail.com>
2026-07-22 20:32:43 +0600

    Add new line for better code formatting

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
+
 const supabaseUrl = "https://wcjbaawyotvzkoslqbcl.supabase.co";
 const supabaseAnonKey = "Sb_publishable_FnTNK485rGVKhgsBYKp2nw_5WnizV5c";
 
