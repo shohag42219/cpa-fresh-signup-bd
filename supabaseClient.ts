bd9ae0b1037abfb9fd96e12e753410a4293b0cc9
shohag42219 <phoneoppo2219@gmail.com>
2026-07-22 20:28:58 +0600

    Add comment for forced redeploy
    Added a comment to indicate a forced redeploy.

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -4,3 +4,5 @@ const supabaseUrl = "https://wcjbaawyotvzkoslqbcl.supabase.co";
 const supabaseAnonKey = "Sb_publishable_FnTNK485rGVKhgsBYKp2nw_5WnizV5c";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// force redeploy
